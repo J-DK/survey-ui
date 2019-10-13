bc5fced0b51454f34f1f3d40797076ed70e13a44
J-DK <dileepkumarjami@gmail.com>
2019-10-13 18:31:02 +0530

    fix(layout): shown the layout only when logged in successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './features/services/auth.service';
 
 @Component({
   selector: 'survey-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'survey-ui';
+
+  constructor(private authService: AuthService) {
+  }
+
+  get show() {
+    return this.authService.currentUserValue ? true : false;
+  }
+
 }
